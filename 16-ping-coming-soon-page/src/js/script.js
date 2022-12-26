fb96558a20bd463dac56b8ad7522ce470b84f448
Annas Dwi <manasdwi276@gmail.com>
2022-12-26 20:32:40 +0700

    if form empty

--- 16-ping-coming-soon-page/src/js/script.js
+++ 16-ping-coming-soon-page/src/js/script.js
@@ -12,22 +12,29 @@ const isEmailValid = (email) => {
   return false;
 };
 
-input.addEventListener("change", (e) => {
+input.addEventListener("input", (e) => {
   const errorCount = document.querySelectorAll("p.error").length;
 
   let p = document.createElement("p");
-  p.textContent = "Please provide a valid email address";
   p.classList.add("error");
 
   input.classList.add("error");
 
-  if (!isEmailValid(e.target.value) && errorCount < 1) {
-    console.log(e.target.parentNode);
+  console.log('Value === ""', e.target.value === "");
+
+  if (e.target.value === "" && errorCount < 1) {
+    p.textContent = "Whoops! It looks like you forgot to add your email";
     buttonEle.parentNode.insertBefore(p, buttonEle);
     return;
   }
 
-  if (isEmailValid(e.target.value) || errorCount > 0) {
+  if (!isEmailValid(e.target.value) && errorCount < 1) {
+    p.textContent = "Please provide a valid email address";
+    buttonEle.parentNode.insertBefore(p, buttonEle);
+    return;
+  }
+
+  if (isEmailValid(e.target.value)) {
     document.querySelector("p.error").remove();
     input.classList.remove("error");
   }
