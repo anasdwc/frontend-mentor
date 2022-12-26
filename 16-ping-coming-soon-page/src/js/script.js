8bbf2633c4e6610fe4ad925d916bc2892cfc4efe
Annas Dwi <manasdwi276@gmail.com>
2022-12-26 19:51:35 +0700

    add mobile style

---
+++ 16-ping-coming-soon-page/src/js/script.js
@@ -0,0 +1,35 @@
+const input = document.querySelector("input#email");
+const inputDiv = document.querySelector(".input");
+const buttonEle = document.querySelector("button");
+
+const isEmailValid = (email) => {
+  const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (email.match(mailFormat)) {
+    return true;
+  }
+
+  return false;
+};
+
+input.addEventListener("change", (e) => {
+  const errorCount = document.querySelectorAll("p.error").length;
+
+  let p = document.createElement("p");
+  p.textContent = "Please provide a valid email address";
+  p.classList.add("error");
+
+  input.classList.add("error");
+
+  if (!isEmailValid(e.target.value) && errorCount < 1) {
+    inputDiv.appendChild(p);
+    return;
+  }
+
+  if (isEmailValid(e.target.value) || errorCount > 0) {
+    document.querySelector("p.error").remove();
+    input.classList.remove("error");
+  }
+});
+
+buttonEle.addEventListener("click", (e) => e.preventDefault());
