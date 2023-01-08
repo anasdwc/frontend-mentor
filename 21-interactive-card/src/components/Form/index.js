a71e2a2c4144353ea93751213cdbf85b28288647
Annas Dwi <manasdwi276@gmail.com>
2023-01-08 07:36:09 +0700

    change grid column input

--- 21-interactive-card/src/components/Form/index.js
+++ 21-interactive-card/src/components/Form/index.js
@@ -18,21 +18,23 @@ function Form() {
           inputType="number"
           inputPlaceholder="e.g. 1234 5678 9123 0000"
         />
-        <FormItem
-          key={3}
-          labelValue="Exp. Date (MM/YY)"
-          inputName={["month", "year"]}
-          inputType="number"
-          inputPlaceholder={["MM", "YY"]}
-          totalInputItem={2}
-        />
-        <FormItem
-          key={4}
-          labelValue="CVC"
-          inputName="cvc"
-          inputType="number"
-          inputPlaceholder="e.g. 123"
-        />
+        <div className="form__column">
+          <FormItem
+            key={3}
+            labelValue="Exp. Date (MM/YY)"
+            inputName={["month", "year"]}
+            inputType="number"
+            inputPlaceholder={["MM", "YY"]}
+            totalInputItem={2}
+          />
+          <FormItem
+            key={4}
+            labelValue="CVC"
+            inputName="cvc"
+            inputType="number"
+            inputPlaceholder="e.g. 123"
+          />
+        </div>
         <button>Confirm</button>
       </form>
     </div>
