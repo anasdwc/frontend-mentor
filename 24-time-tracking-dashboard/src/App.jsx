7fa957b5fbccef282eda90579256e4984224f4f0
Annas Dwi <manasdwi276@gmail.com>
2023-01-14 08:17:50 +0700

    add project 24

---
+++ 24-time-tracking-dashboard/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
