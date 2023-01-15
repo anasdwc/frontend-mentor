e530a55603e8d33f759e7a38df5ef4d83fdaa0ef
Annas Dwi <manasdwi276@gmail.com>
2023-01-15 18:18:02 +0700

    add 25 projects

---
+++ 25-tip-calculator/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer class="attribution">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by <a href="https://www.github.com/anasdwc">Annas Dwi</a>.
+    </footer>
+  );
+}
+
+export default Footer;
