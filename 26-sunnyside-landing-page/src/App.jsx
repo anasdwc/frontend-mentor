89f1266561fe1931b5853c7ca5637aa6a96955a3
Annas Dwi <manasdwi276@gmail.com>
2023-01-21 18:46:01 +0700

    remove styled-components

--- 26-sunnyside-landing-page/src/App.jsx
+++ 26-sunnyside-landing-page/src/App.jsx
@@ -1,23 +1,12 @@
-import styled from "styled-components";
-import Footer from "./components/Footer";
-
-const Title = styled.h1`
-  font-size: 1.5rem;
-  text-align: center;
-  color: palevioletred;
-`;
-
-const Wrapper = styled.div`
-  padding: 4rem;
-  background: papayawhip;
-`;
+import React from "react";
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello World</Title>
-      <Footer />
-    </Wrapper>
+    <>
+      <header>
+        <h1>Hello World</h1>
+      </header>
+    </>
   );
 }
 
