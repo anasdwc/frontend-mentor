5d540e5eac2a59c11a6d56bf1d20b551dfca3225
Annas Dwi <manasdwi276@gmail.com>
2023-01-24 08:23:21 +0700

    add nav animation

--- 27-intro-dropdown/src/App.jsx
+++ 27-intro-dropdown/src/App.jsx
@@ -1,14 +1,19 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Body from "./components/Body";
 
 import "@fontsource/epilogue/500.css";
 import "@fontsource/epilogue/700.css";
-import Body from "./components/Body";
+import { useState } from "react";
 
 function App() {
+  const [isCollapsed, setIsCollapsed] = useState(true);
   return (
-    <div className="App">
-      <Header />
+    <div className="App overflow-hidden">
+      <Header
+        isCollapsed={isCollapsed}
+        onClick={() => setIsCollapsed(!isCollapsed)}
+      />
       <Body />
       <Footer />
     </div>
--- 27-intro-dropdown/src/components/Header.jsx
+++ 27-intro-dropdown/src/components/Header.jsx
@@ -1,13 +1,99 @@
-export default function Header() {
+import clsx from "clsx";
+
+export default function Header({ isCollapsed, onClick }) {
   return (
     <header>
-      <nav className="flex justify-between px-4 py-6">
+      <nav className="relative flex items-center justify-between px-4 py-6 pt-3">
         <div id="brand">
           <img src="./images/logo.svg" alt="" />
         </div>
-        <div id="burger">
-          <button>
-            <img src="./images/icon-menu.svg" alt="" />
+        <div id="nav-list">
+          <div
+            className={clsx(
+              "absolute inset-0 h-screen w-screen bg-almost-black opacity-60 transition-opacity duration-500",
+              isCollapsed && "opacity-0"
+            )}
+          ></div>
+          <ul
+            className={clsx(
+              "absolute  right-0 top-0 flex h-screen w-[60vw]  flex-col gap-4 bg-almost-white  pl-6 pt-20 pr-4 text-sm text-medium-gray transition-transform duration-500",
+              isCollapsed && "translate-x-64 ",
+              "[&>li]:cursor-pointer"
+            )}
+          >
+            <li
+              className={clsx(
+                "[&>div>img]:hover:rotate-180 [&>div>img]:hover:transition-transform [&>div>img]:hover:duration-500",
+                "[&_ul]:hover:relative [&_ul]:hover:z-10 [&_ul]:hover:flex [&_ul]:hover:translate-y-0 [&_ul]:hover:opacity-100 [&_ul]:hover:transition-all [&_ul]:hover:duration-500"
+              )}
+            >
+              <div className="flex items-center gap-3">
+                Features{" "}
+                <img
+                  className={clsx("h-[.4rem]")}
+                  src="./images/icon-arrow-down.svg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <ul
+                  className={clsx(
+                    "absolute -z-10 flex -translate-y-20 flex-col gap-4 py-4 pl-6 opacity-0",
+                    "[&_li]:flex [&_li]:items-center [&_li]:gap-3"
+                  )}
+                >
+                  <li>
+                    <img src="./images/icon-todo.svg" alt="" /> Todo List
+                  </li>
+                  <li>
+                    <img src="./images/icon-calendar.svg" alt="" /> Calendar
+                  </li>
+                  <li>
+                    <img src="./images/icon-reminders.svg" alt="" /> Reminders
+                  </li>
+                  <li>
+                    <img src="./images/icon-planning.svg" alt="" /> Planning
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li
+              className={clsx(
+                "[&_img]:hover:rotate-180 [&_img]:hover:transition-transform [&_img]:hover:duration-500",
+                "[&_ul]:hover:relative [&_ul]:hover:z-10 [&_ul]:hover:flex [&_ul]:hover:translate-y-0 [&_ul]:hover:opacity-100 [&_ul]:hover:transition-all [&_ul]:hover:duration-500"
+              )}
+            >
+              <div className="flex items-center gap-3">
+                Company{" "}
+                <img
+                  className={clsx("h-[.4rem]")}
+                  src="./images/icon-arrow-down.svg"
+                  alt=""
+                />
+              </div>
+              <div>
+                <ul className="absolute -z-10 flex -translate-y-20 flex-col gap-4 py-4 pl-6 opacity-0">
+                  <li>History</li>
+                  <li>Our Team</li>
+                  <li>Blog</li>
+                </ul>
+              </div>
+            </li>
+            <li>Careers</li>
+            <li>About</li>
+            <li className="mt-2 w-full text-center">Login</li>
+            <li className="mr-4 w-full rounded-xl border-2 border-medium-gray  py-2 text-center">
+              Register
+            </li>
+          </ul>
+        </div>
+        <div id="burger" className="relative z-10">
+          <button onClick={onClick} className="h-10">
+            {isCollapsed ? (
+              <img src="./images/icon-menu.svg" alt="" />
+            ) : (
+              <img src="./images/icon-close-menu.svg" alt="" />
+            )}
           </button>
         </div>
       </nav>
