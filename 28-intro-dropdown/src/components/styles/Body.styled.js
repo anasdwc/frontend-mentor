b0f5871e1dc65be2976147f7f5b85da4af925e11
Annas Dwi <manasdwi276@gmail.com>
2023-01-25 12:22:38 +0700

    remove 28 projects

--- 28-intro-dropdown/src/components/styles/Body.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Paragraph = styled.p`
-  color: ${({ theme }) => theme.colors.gray};
-  text-align: center;
-  padding: 0 1rem;
-  line-height: 1.5;
-`;
