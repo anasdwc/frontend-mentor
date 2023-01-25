c362b4f56b8eb69fbd984b46ff3ba7d2358c90d3
Annas Dwi <manasdwi276@gmail.com>
2023-01-25 11:27:20 +0700

    add styled

--- 28-intro-dropdown/src/App.jsx
+++ 28-intro-dropdown/src/App.jsx
@@ -1,12 +1,26 @@
+import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
 import { Container } from "./components/styles/Container.styled";
+import GlobalStyles from "./components/styles/Global";
+
+const theme = {
+  colors: {
+    white: "hsl(0, 0%, 98%)",
+    gray: "hsl(0, 0%, 41%)",
+    black: "hsl(0, 0%, 8%)",
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <h1>Hello World</h1>
-      </Container>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Container>
+          <Header />
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ 28-intro-dropdown/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { StyledHeader } from "./styles/StyledHeader.styled";
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <h1>Hello World</h1>
+    </StyledHeader>
+  );
+}
