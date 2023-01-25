513f2062dd2e8601e52273e5f81d29f12cdd3c4a
Annas Dwi <manasdwi276@gmail.com>
2023-01-25 11:07:00 +0700

    add 28 projects

---
+++ 28-intro-dropdown/src/App.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./components/styles/Container.styled";
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
