cb3ea8e10d83cd55d71772d206f2f9ff73009758
Annas Dwi <manasdwi276@gmail.com>
2023-01-26 18:55:48 +0700

    add header

--- 27-blogr/src/App.jsx
+++ 27-blogr/src/App.jsx
@@ -1,7 +1,15 @@
+import Header from "./components/Header";
+
+import "@fontsource/overpass/300.css";
+import "@fontsource/overpass/600.css";
+import "@fontsource/ubuntu/400.css";
+import "@fontsource/ubuntu/500.css";
+import "@fontsource/ubuntu/700.css";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
     </div>
   );
 }
--- 27-blogr/tailwind.config.cjs
+++ 27-blogr/tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Ubuntu",
+        overpass: "Overpass",
+      },
+      colors: {
+        "light-red": "hsl(356, 100%, 66%)",
+        "very-light-red": "hsl(355, 100%, 74%)",
+        "very-dark-blue": "hsl(208, 49%, 24%)",
+        "grayish-blue": "hsl(240, 2%, 79%)",
+        "very-dark-grayish-blue": " hsl(207, 13%, 34%)",
+        "very-dark-black-blue": "hsl(240, 10%, 16%)",
+        "gradient-very-light-red": "hsl(13, 100%, 72%)",
+        "gradient-light-red": "hsl(353, 100%, 62%)",
+      },
+      backgroundImage: {
+        "hero-pattern-mobile": "url('./images/bg-pattern-intro-mobile.svg')",
+        "hero-pattern-desktop": "url('./images/bg-pattern-intro-desktop.svg')",
+      },
+    },
   },
   plugins: [],
 };
