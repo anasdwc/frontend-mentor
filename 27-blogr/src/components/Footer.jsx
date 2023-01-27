48079188569d1e18bcdd63ce8a206c6e41dab058
Annas Dwi <manasdwi276@gmail.com>
2023-01-27 09:02:18 +0700

    desktop footer

--- 27-blogr/src/components/Footer.jsx
+++ 27-blogr/src/components/Footer.jsx
@@ -1,7 +1,9 @@
+import clsx from "clsx";
+
 function MenuList({ title, itemList }) {
   return (
-    <ul className="my-12 flex flex-col gap-1 text-center last:mb-0">
-      <li className="mb-4 font-bold opacity-80">{title}</li>
+    <ul className="my-12 flex flex-col gap-1 text-center text-white last:mb-0 md:my-0 md:text-left">
+      <li className="mb-4 font-bold opacity-80 ">{title}</li>
       {itemList.map((item) => (
         <li className="opacity-50">{item}</li>
       ))}
@@ -11,35 +13,38 @@ function MenuList({ title, itemList }) {
 
 function Footer() {
   return (
-    <footer className="flex flex-col items-center rounded-tr-[5rem] bg-very-dark-black-blue py-16">
+    <footer
+      className={clsx(
+        "flex flex-col items-center rounded-tr-[5rem] bg-very-dark-black-blue py-16",
+        " md:flex-row md:items-start md:justify-between md:pr-80 md:pl-44"
+      )}
+    >
       <div>
         <img src="./images/logo.svg" alt="" />
       </div>
-      <div className="text-white">
-        <div>
-          <MenuList
-            title="Product"
-            itemList={[
-              "Overview",
-              "Pricing",
-              "Marketplace",
-              "Features",
-              "Integrations",
-            ]}
-          />
-        </div>
-        <div>
-          <MenuList
-            title="Company"
-            itemList={["About", "Team", "Blog", "Careers"]}
-          />
-        </div>
-        <div>
-          <MenuList
-            title="Connect"
-            itemList={["Contact", "Newsletetr", "LinkedIn"]}
-          />
-        </div>
+      <div>
+        <MenuList
+          title="Product"
+          itemList={[
+            "Overview",
+            "Pricing",
+            "Marketplace",
+            "Features",
+            "Integrations",
+          ]}
+        />
+      </div>
+      <div>
+        <MenuList
+          title="Company"
+          itemList={["About", "Team", "Blog", "Careers"]}
+        />
+      </div>
+      <div>
+        <MenuList
+          title="Connect"
+          itemList={["Contact", "Newsletetr", "LinkedIn"]}
+        />
       </div>
       {/* <div class="attribution">
         Challenge by{" "}
