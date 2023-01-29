58a80fca40634da8ed846bee9881a6be765ddf41
Annas Dwi <manasdwi276@gmail.com>
2023-01-29 18:02:05 +0700

    desktop on article 1

--- 27-blogr/src/components/Article.jsx
+++ 27-blogr/src/components/Article.jsx
@@ -1,6 +1,13 @@
-function Post({ title, paragraph }) {
+import clsx from "clsx";
+
+function Post({ title, paragraph, extendedClass }) {
   return (
-    <div className="px-8 text-center">
+    <div
+      className={clsx(
+        "px-8 text-center md:text-left",
+        extendedClass && extendedClass
+      )}
+    >
       <h3 className="font-overpass text-2xl font-[600] text-very-dark-blue">
         {title}
       </h3>
@@ -13,17 +20,21 @@ function Post({ title, paragraph }) {
 
 function ArticleOne() {
   return (
-    <section className="my-24 flex flex-col items-center">
-      <div>
-        <h2 className="text-center font-overpass text-2xl font-[600] text-very-dark-blue">
-          Designed for the future
-        </h2>
+    <section className="my-24 flex flex-col items-center md:relative md:my-44 md:grid md:grid-cols-2 md:grid-rows-[.5fr_1fr_1fr] md:gap-y-8 md:px-44">
+      <h2 className="text-center font-overpass text-2xl font-[600] text-very-dark-blue md:col-span-full md:mb-12 md:text-4xl">
+        Designed for the future
+      </h2>
+      <picture className="md:relative md:col-start-2 md:col-end-3 md:row-start-2 md:row-end-4">
+        <source
+          media="(min-width: 768px)"
+          srcset="./images/illustration-editor-desktop.svg"
+        />
         <img
           src="./images/illustration-editor-mobile.svg"
           alt=""
-          className="my-12 w-full"
+          className="my-12 w-full md:absolute md:inset-y-0 md:left-20 md:my-auto md:min-w-[60rem]"
         />
-      </div>
+      </picture>
       <Post
         title="Introducing an extensible editor"
         paragraph="Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog"
@@ -31,6 +42,7 @@ function ArticleOne() {
       <Post
         title="Robust content management"
         paragraph="Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this functionality, you're in full control"
+        extendedClass="md:col-start-1 md:col-end-2"
       />
     </section>
   );
