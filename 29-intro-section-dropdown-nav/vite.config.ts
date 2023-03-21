c9676bddd1916c437ffc025734db9ce5a3289cfe
Annas Dwi <manasdwi276@gmail.com>
2023-03-21 08:44:28 +0700

    fix name plugin

--- 29-intro-section-dropdown-nav/vite.config.ts
+++ 29-intro-section-dropdown-nav/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import VitePluginFonts from "vite-plugin-fonts";
+import { VitePluginFonts } from "vite-plugin-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
