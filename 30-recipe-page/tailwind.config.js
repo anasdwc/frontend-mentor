3a861f54e266982570142d61339b4b9e3b7486ca
anasdwc <mannasdwc@gmail.com>
2024-10-20 09:54:56 +0700

    add recipe page

---
+++ 30-recipe-page/tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Outfit', ...defaultTheme.fontFamily.sans],
+                head: ['Young Serif', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'stone-100': 'hsl(30, 54%, 90%)',
+                'stone-150': 'hsl(30, 18%, 87%)',
+                'stone-600': 'hsl(30, 10%, 34%)',
+                'stone-900': 'hsl(24, 5%, 18%)',
+                'brown-800': 'hsl(14, 45%, 36%)',
+                'rose-800': 'hsl(332, 51%, 32%)',
+                'rose-50': 'hsl(330,100%,98%)',
+            },
+        },
+        container: {
+            center: true,
+        },
+    },
+    plugins: [],
+}
